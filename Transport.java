7dee57741019116eaaca43cc972efe34678f9f04
Roman Olmezov <romcha@ymail.com>
2023-06-19 10:52:17 +0800

    Добавлен класс Transport

--- Transport.java
+++ Transport.java
@@ -1,6 +1,29 @@
 import java.util.*;
 
-public class Transport() {
-	public interface commercial {}
+public class Transport {
+	public static void main(String[] args) {
 
+	Bus b = new Bus();
+	b.maxSpeed = 500.4;
+	System.out.println(b.toString() + b.maxSpeed);
+
+	}
+
+	public interface commercial {}
+	public interface personal {}
+
+	public interface land {
+		
+	}
+	public interface air {}
+	public interface sea {}
+
+	
+
+
+
+}
+
+class Bus implements Transport.land, Transport.commercial {
+	double maxSpeed = 0.0;
 }
